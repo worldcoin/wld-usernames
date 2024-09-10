cb8ccc0f9a577ade07547ff9de76a105d2fe15c6
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-09-10 17:32:57 +0200

    Add health (#3)
    * health
    
    * add health check

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,16 @@
-use aide::axum::ApiRouter;
+use aide::axum::{routing::get_with, ApiRouter};
 
 mod api;
 mod docs;
+mod health;
 mod system;
+
+use health::{docs as health_docs, health};
 
 pub fn handler() -> ApiRouter {
 	ApiRouter::new()
 		.merge(docs::handler())
 		.merge(system::handler())
+		.api_route("/health", get_with(health, health_docs))
 		.nest("/api", api::handler())
 }
