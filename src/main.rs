91dbc0a6414b545c2dc052653258e54e26e78468
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-15 15:39:07 -0700

    add logs (#9)
    * add logs
    
    * change init
    
    * modify init

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,31 @@
 use anyhow::Result;
 use config::Config;
 use dotenvy::dotenv;
-use tracing_subscriber::{
-	prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer,
-};
 
 mod blocklist;
 mod config;
-mod verify;
 mod routes;
 mod server;
 mod types;
 mod utils;
+mod verify;
 
 #[tokio::main]
 async fn main() -> Result<()> {
 	dotenv().ok();
 
-	tracing_subscriber::registry()
-		.with(tracing_subscriber::fmt::layer().with_filter(
-			EnvFilter::try_from_default_env().unwrap_or_else(|_| "wld_usernames=info".into()),
-		))
+	tracing_subscriber::fmt()
+		.json()
+		.with_target(false)
+		.flatten_event(true)
 		.init();
+
+	tracing::info!("👩 Server started");
 
 	let config = Config::from_env().await?;
 	config.migrate_database().await?;
+
+	tracing::info!("👩‍🌾 Migrations run");
 
 	server::start(config).await
 }
