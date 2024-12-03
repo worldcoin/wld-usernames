014ce3e18f049c32e57e4d0a4a54d16eb1f940b0
Michal Struck <michal.o.struck@gmail.com>
2024-12-03 15:55:25 +0100

    fix: lowercase search strings, validate username

--- src/routes/api/v1/search.rs
+++ src/routes/api/v1/search.rs
@@ -1,5 +1,5 @@
 use crate::{
-	config::Db,
+	config::{Db, USERNAME_SEARCH_REGEX},
 	types::{ErrorResponse, Name, UsernameRecord},
 };
 use axum::{
@@ -13,13 +13,19 @@ pub async fn search(
 	Extension(db): Extension<Db>,
 	Path(username): Path<String>,
 ) -> Result<Response, ErrorResponse> {
+	let lowercase_username = username.to_lowercase();
+
+	if !USERNAME_SEARCH_REGEX.is_match(&lowercase_username) {
+		return Ok(Json(Vec::<UsernameRecord>::new()).into_response());
+	}
+
 	let names = sqlx::query_as!(
 		Name,
 		"SELECT * FROM names
 			WHERE username % $1
 			ORDER BY similarity(username, $1) DESC
 			LIMIT 10;",
-		username
+		lowercase_username
 	)
 	.fetch_all(&db.read_only)
 	.await?;
@@ -34,6 +40,6 @@ pub async fn search(
 }
 
 pub fn docs(op: aide::transform::TransformOperation) -> aide::transform::TransformOperation {
-	op.description("Search for up to 10 usernames")
+	op.description("Search for up to 10 usernames. Accepts 1 to 14, only valid username characters to search with.")
 		.response::<200, Json<Vec<UsernameRecord>>>()
 }
