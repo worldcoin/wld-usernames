6a41fa79833f3121dfd6d9454fdd0e1942105e8c
Konstantin Borisov <konstantin.borisov@toolsforhumanity.com>
2024-12-09 13:15:04 +0100

    some traces are coming

--- src/server.rs
+++ src/server.rs
@@ -3,10 +3,13 @@ use anyhow::Result;
 use axum::Extension;
 use std::{env, net::SocketAddr};
 use tokio::{net::TcpListener, signal};
+use tower_http::trace::TraceLayer;
+use uuid::Uuid;
 
 use crate::{config::Config, routes};
 
 pub async fn start(mut config: Config) -> Result<()> {
+	// Create OpenAPI info
 	let mut openapi = OpenApi {
 		info: openapi::Info {
 			title: "World App Username API".to_string(),
@@ -16,13 +19,42 @@ pub async fn start(mut config: Config) -> Result<()> {
 		..OpenApi::default()
 	};
 
+	// Define the router with the necessary layers
 	let router = routes::handler()
 		.finish_api(&mut openapi)
 		.layer(Extension(openapi))
 		.layer(config.db_extension())
 		.layer(config.blocklist_extension())
-		.layer(config.extension());
+		.layer(config.extension())
+		.layer(
+			TraceLayer::new_for_http()
+				.on_request(|request: &http::Request<_>, _span: &tracing::Span| {
+					tracing::info!(method = %request.method(), uri = %request.uri(), "received request");
+				})
+				.on_response(
+					|response: &http::Response<_>,
+					 latency: std::time::Duration,
+					 _span: &tracing::Span| {
+						tracing::info!(status = response.status().as_u16(), latency = ?latency, "response sent");
+					},
+				)
+				.make_span_with(|request: &http::Request<_>| {
+					let trace_id = Uuid::new_v4().to_string();
 
+					// println!("	: {}", trace_id);
+
+					tracing::info!("Extracted trace_id: {}", trace_id);
+
+					tracing::info_span!(
+						"http_request",
+						method = %request.method(),
+						uri = %request.uri(),
+						trace_id = %trace_id
+					)
+				}),
+		);
+
+	// Server setup and binding
 	let addr = SocketAddr::from((
 		[0, 0, 0, 0],
 		env::var("PORT").map_or(Ok(8000), |p| p.parse())?,
