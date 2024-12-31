cf15e41bb4428657987d3b7cd811eab09850ee60
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-12-31 11:05:41 -0800

    remove unique

--- migrations/20241230195405_add_case_insensitive_username_indexes.sql
+++ migrations/20241230195405_add_case_insensitive_username_indexes.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
-CREATE UNIQUE INDEX names_username_lower_idx ON names (LOWER(username));
+CREATE INDEX names_username_lower_idx ON names (LOWER(username));
 
-CREATE UNIQUE INDEX old_names_username_lower_idx ON old_names (LOWER(old_username));
+CREATE INDEX old_names_username_lower_idx ON old_names (LOWER(old_username));
