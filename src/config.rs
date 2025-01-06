1bf41e1ad58580022271faddee1310c3babf5967
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-01-06 12:37:05 -0800

    switch connection

--- src/config.rs
+++ src/config.rs
@@ -60,6 +60,7 @@ impl Config {
 		);
 
 		let db_client = PgPoolOptions::new()
+			.max_connections(100)
 			.acquire_timeout(Duration::from_secs(3))
 			.connect(
 				&env::var("DATABASE_URL").context("DATABASE_URL environment variable not set")?,
