b31b204d6155aac8ce529be7eada3c22716c668a
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-01-06 12:35:51 -0800

    create max connections (#54)

--- src/config.rs
+++ src/config.rs
@@ -67,6 +67,7 @@ impl Config {
 			.await?;
 
 		let db_read_client = PgPoolOptions::new()
+			.max_connections(100)
 			.acquire_timeout(Duration::from_secs(3))
 			.connect(
 				&env::var("DATABASE_READ_URL")
