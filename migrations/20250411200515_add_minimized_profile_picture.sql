4bd3afed975ae504bd8e21d6d15ee1bdd1ec6f16
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-04-14 12:38:21 -0700

    Drop old index after creating new one

--- migrations/20250411200515_add_minimized_profile_picture.sql
+++ migrations/20250411200515_add_minimized_profile_picture.sql
@@ -4,4 +4,6 @@
 ALTER TABLE names ADD COLUMN minimized_profile_picture_url VARCHAR(255);
 
 -- Create a new index that includes both profile picture URLs
-CREATE INDEX IF NOT EXISTS username_trgm_with_pictures_idx ON names USING GIST (username gist_trgm_ops(siglen=256)) INCLUDE (address, profile_picture_url, minimized_profile_picture_url);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS username_trgm_with_pictures_idx ON names USING GIST (username gist_trgm_ops(siglen=256)) INCLUDE (address, profile_picture_url, minimized_profile_picture_url);
+
+DROP INDEX IF EXISTS username_trgm_idx;
