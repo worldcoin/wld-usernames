f783596cf4d86d79f751cbf0de190bd74c49fcdf
Dmitry Lugovoy <dmitry.lugovoy@toolsforhumanity.com>
2025-04-22 15:17:52 -0700

    Replace regex with more performant operation

--- migrations/20250422215826_fill_minimized_profile_picture.sql
+++ migrations/20250422215826_fill_minimized_profile_picture.sql
@@ -1,6 +1,5 @@
 UPDATE names
 SET minimized_profile_picture_url = 
-  regexp_replace(profile_picture_url, 
-                 '/([^/]+)$', 
-                 '/minimized_\1')
+  left(profile_picture_url, length(profile_picture_url) - length(split_part(profile_picture_url, '/', -1))) ||
+  'minimized_' || split_part(profile_picture_url, '/', -1)
 WHERE profile_picture_url IS NOT NULL;
